34b0e8f5c261540d586fe0c07e3efaf8d4040eee
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-01-03 15:36:20 +0100

    Refresh in nicer way, using React's setTime and recreating URL, not by window.location.reload

--- src/plugins/backgrounds/online/Online.tsx
+++ src/plugins/backgrounds/online/Online.tsx
@@ -2,35 +2,34 @@ import React from "react";
 import Backdrop from "../../../views/shared/Backdrop";
 import "./Online.sass";
 import { defaultData, Props } from "./types";
-
-/* Refresh the page, to refresh image, every 15 minutes.
-  Using setTimeout, regular JS,
-  https://developer.mozilla.org/en-US/docs/Web/API/Window/setTimeout
-
-  TODO: There's probably a better way to do this,
-  and refresh only image contents, with React.
-  The Unsplash plugin uses useRotatingCache.
-*/
-const timeout = 15 * 60 * 1000;
-function keepRefreshingPage(): void
-{
-  window.location.reload();
-  setTimeout(keepRefreshingPage, timeout);
-}
-
-setTimeout(keepRefreshingPage, timeout);
+import { useTime } from "../../../hooks";
 
 const Online: React.FC<Props> = ({ data = defaultData }) => {
   if (!data.url) return <div className="Online default fullscreen" />;
 
-  let url = data.url;
-  // Append random number to url to bypass browser cache.
-  // Likely unnecessary in the end -- this was a test trying to workaround
-  // update problems.
-  url += (url.includes("?") ? "&" : "?") +
-    "random_to_avoid_browser_cache=" + Math.floor(Math.random() * 1000 * 1000);
+  const timeout = 15 * 60 * 1000;
+  // debug: change every 1 second, 10 seconds
+  // const timeout = 1000;
+  // const timeout = 10 * 1000;
+  const time = useTime("absolute");
 
-  console.log("Online background from: " + url);
+  let url = data.url;
+
+  // Modify URL once timeout passes, to reload the image after such time.
+  // This code is evaluated in a loop, so to load a new image, changing
+  // URL is all we need.
+  let milisecs = time.getTime();
+    //Date.now(); // it is important to use useTime, to keep this rerendering
+  milisecs = Math.floor(milisecs / timeout) * timeout;
+  url += (url.includes("?") ? "&" : "?") + "timeout=" + milisecs;
+
+  // Note: This would be bad, refreshing image as often as possible.
+  // At least sometimes it would refresh often...
+  // url += (url.includes("?") ? "&" : "?") +
+  //   "random_to_avoid_browser_cache=" + Math.floor(Math.random() * 1000 * 1000);
+
+  // Do not keep logging, as this occurs often, this is reevaluated often.
+  //console.log("Online background from: " + url);
 
   return (
     <Backdrop
